743a485120899430ae57d32882f1d9facd802d90
pehgs <pehgs@tarc.edu.my>
2021-12-08 16:45:32 +0800

    update get function

--- app/src/main/java/com/example/uploadimage/MainActivity.kt
+++ app/src/main/java/com/example/uploadimage/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.uploadimage
 
 import android.app.Activity
 import android.content.Intent
+import android.graphics.BitmapFactory
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -9,7 +10,16 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.graphics.createBitmap
+import com.bumptech.glide.Glide
 import com.google.firebase.storage.FirebaseStorage
+import java.io.File
+import androidx.annotation.NonNull
+
+import com.google.android.gms.tasks.OnCompleteListener
+
+import com.google.firebase.storage.StorageReference
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var img: ImageView
@@ -24,6 +34,7 @@ class MainActivity : AppCompatActivity() {
 
         val btnBrowse: Button = findViewById(R.id.btnBrowse)
         val btnUpload: Button = findViewById(R.id.btnUpload)
+        val btnGet: Button = findViewById(R.id.btnGet)
 
         btnBrowse.setOnClickListener() {
 
@@ -36,7 +47,7 @@ class MainActivity : AppCompatActivity() {
 
         btnUpload.setOnClickListener() {
 
-            val fName = "sdfsfd"
+            val fName = "abc.jpg"
 
             val storageRef = FirebaseStorage.getInstance().getReference("images/${fName}")
 
@@ -49,6 +60,44 @@ class MainActivity : AppCompatActivity() {
                 .addOnFailureListener {
                     Toast.makeText(applicationContext, it.toString(), Toast.LENGTH_LONG).show()
                 }
+        }
+
+        btnGet.setOnClickListener() {
+
+            val fName = "abc.jpg"
+//
+//            val storageRef = FirebaseStorage.getInstance().getReference("images/${fName}")
+//
+//            val file = File.createTempFile("temp", "jpg")
+//
+//            storageRef.getFile(file)
+//                .addOnSuccessListener {
+//                    val bitmap = BitmapFactory.decodeFile(file.absolutePath)
+//                    img.setImageBitmap(bitmap)
+//                }
+//                .addOnFailureListener {
+//                    Toast.makeText(applicationContext, it.toString(), Toast.LENGTH_LONG).show()
+//                }
+
+
+            val ref = FirebaseStorage.getInstance().getReference("images")
+                .child("${fName}")
+
+            ref.downloadUrl
+                .addOnCompleteListener {
+
+                        val downUri: Uri? = it.getResult()
+                        val imageUrl = downUri.toString()
+
+                        Glide.with(img.context)
+                            .load(imageUrl)
+                            .into(img)
+
+                }.addOnFailureListener{
+
+                }
+
+
         }
 
     }
